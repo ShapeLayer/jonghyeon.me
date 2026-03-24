c01cd6e548439912235cf34776efa1865d2a1eb0
Jonghyeon Park <shapelayer.dev@gmail.com>
2026-03-24 23:05:28 +0900

    chore: support cf pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,17 @@
-import adapter from '@sveltejs/adapter-static';
+import adapterCloudflare from '@sveltejs/adapter-cloudflare';
+import adapterStatic from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const isCloudflareBuild = process.env.DEPLOY_TARGET === 'cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-  kit: { adapter: adapter() }
+  kit: {
+    adapter: isCloudflareBuild ? adapterCloudflare() : adapterStatic()
+  }
 };
 
 export default config;
