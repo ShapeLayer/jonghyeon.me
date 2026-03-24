4bfd9eb63017d3e5dad44864dfa6d8fc7cb6fd3f
Jonghyeon Park <shapelayer.dev@gmail.com>
2026-03-24 23:26:38 +0900

    Revert "chore: support cf pages"
    This reverts commit c01cd6e548439912235cf34776efa1865d2a1eb0.

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,12 @@
-import adapterCloudflare from '@sveltejs/adapter-cloudflare';
-import adapterStatic from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-const isCloudflareBuild = process.env.DEPLOY_TARGET === 'cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-  kit: {
-    adapter: isCloudflareBuild ? adapterCloudflare() : adapterStatic()
-  }
+  kit: { adapter: adapter() }
 };
 
 export default config;
