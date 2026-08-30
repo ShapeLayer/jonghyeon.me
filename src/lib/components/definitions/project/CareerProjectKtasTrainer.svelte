66916fcf361cda541f963fdb0cc19c577873f189
Jonghyeon Park <jng.hyn.park@gmail.com>
2026-08-30 18:09:36 +0900

    content: add detail information about ktas-trainer

--- src/lib/components/definitions/project/CareerProjectKtasTrainer.svelte
+++ src/lib/components/definitions/project/CareerProjectKtasTrainer.svelte
@@ -3,4 +3,9 @@
   import CareerItem from '$lib/components/CareerItem.svelte';
 </script>
 
-<CareerItem id='project-ktas-trainer' title={m.career_title_project_ktas_trainer()} />
+<CareerItem id='project-ktas-trainer' title={m.career_title_project_ktas_trainer()}>
+  <ul>
+    <li>{m.career_details_project_ktas_trainer_li_0()}</li>
+    <li>{m.career_details_project_ktas_trainer_li_1()}</li>
+  </ul>
+</CareerItem>
