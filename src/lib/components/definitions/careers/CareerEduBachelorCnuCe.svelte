34e076b922c3e8e73765b261eaf0eb47635fd0cc
Jonghyeon Park <jng.hyn.park@gmail.com>
2026-08-30 18:13:33 +0900

    content: link ShapeLayer/computer-engineering-undergraduate-program

--- src/lib/components/definitions/careers/CareerEduBachelorCnuCe.svelte
+++ src/lib/components/definitions/careers/CareerEduBachelorCnuCe.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
   import { m } from '$lib/paraglide/messages';
   import CareerItem from '$lib/components/CareerItem.svelte';
+	import ExternalLink from '$lib/components/ExternalLink.svelte';
 </script>
 
 <CareerItem
   id='edu-bachelor-chonnam-natl-univ-ce'
   title={m.career_title_edu_bachelor_cnu_ce()}
-/>
+>
+  <ul>
+    <li><ExternalLink href="https://github.com/ShapeLayer/computer-engineering-undergraduate-program">GitHub</ExternalLink></li>
+  </ul>
+</CareerItem>
